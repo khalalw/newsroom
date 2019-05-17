f40a257bfacf5f6b798b7408be89afee1fb8db53
Khalal Walker <khalal@clearsparc.com>
2019-05-17 14:37:36 -0700

    initial setup

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-toolbar dark>
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title>The Newsroom</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>Top Stories</v-btn>
+      <v-btn flat>Latest Stories</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
