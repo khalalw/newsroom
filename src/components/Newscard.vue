61bffed1c76ab8419fa264c58b26fe0eef08e0d0
Author Name <khalal.walker@gmail.com>
2019-05-17 14:37:36 -0700

    initial setup

---
+++ src/components/Newscard.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-layout>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-img class="white--text" width="400px" :src="image"></v-img>
+        <v-card-title>
+          <div>
+            <span class="grey--text">Number 10</span>
+            <br>
+            <span>Whitehaven Beach</span>
+            <br>
+            <span>Whitsunday Island, Whitsunday Islands</span>
+          </div>
+        </v-card-title>
+        <v-card-actions>
+          <v-btn flat color="orange">View Source</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  props: ["newsTitle", "publishTime", "image", "description", "articleURL"]
+};
+</script>
+
+<style>
+</style>
