1eeffae695b85e056dd2baeecd29f43dfe76ac4b
Khalal Walker <khalal@clearsparc.com>
2019-05-20 22:04:45 -0700

    set up routes

--- src/main.js
+++ src/main.js
@@ -1,11 +1,25 @@
 import Vue from 'vue';
 import './plugins/vuetify';
 import App from './App.vue';
-import { VueMasonryPlugin } from 'vue-masonry';
+import VueRouter from 'vue-router';
+
+import Top from './pages/Top';
 
 Vue.config.productionTip = false;
-Vue.use(VueMasonryPlugin);
+Vue.use(VueRouter);
+
+// router
+const routes = [
+  { path: 'top', component: Top },
+  { path: '/', redirect: '/top' }
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+});
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app');
