8b6314f44a97285651288ce53835d01e479ff99e
Author Name <khalal.walker@gmail.com>
2019-05-20 22:04:45 -0700

    set up routes

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,28 @@
 <template>
   <v-app id="inspire" dark>
     <Navbar @toggle-nav="toggleNav"/>
-    <Sidenav :drawer="drawer"/>
-
+    <!-- drawer -->
+    <v-navigation-drawer clipped fixed v-model="drawer" app>
+      <v-list dense>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>dashboard</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Top Stories</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Settings</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <!-- drawer -->
     <v-content>
       <v-container fill-height>
         <v-layout justify-center align-center>
@@ -14,11 +34,10 @@
 </template>
 
 <script>
-import Sidenav from "./components/Sidenav";
 import Navbar from "./components/Navbar";
 import Top from "./pages/Top";
 export default {
-  components: { Sidenav, Navbar, Top },
+  components: { Navbar, Top },
   data: () => ({
     drawer: null
   }),
