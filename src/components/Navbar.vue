64dd133222b9b991aaad8d65dec1f47c90efae24
Author Name <khalal.walker@gmail.com>
2019-05-22 11:41:43 -0700

    update formatting

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,28 +1,28 @@
 <template>
-  <v-toolbar app fixed clipped-left>
+  <v-toolbar app clipped-left fixed>
     <v-toolbar-side-icon @click="toggleSideBar()"></v-toolbar-side-icon>
     <v-toolbar-title>The Newsroom</v-toolbar-title>
     <v-spacer></v-spacer>
-    <v-layout row align-center style="max-width: 275px" class="hidden-md-and-down">
+    <v-layout align-center class="hidden-md-and-down" row style="max-width: 275px">
       <v-text-field
-        placeholder="Search..."
-        single-line
         append-icon="search"
         color="white"
         hide-details
+        placeholder="Search..."
+        single-line
       ></v-text-field>
     </v-layout>
   </v-toolbar>
 </template>
 
 <script>
-export default {
-  methods: {
-    toggleSideBar() {
-      this.$emit("toggle-nav");
+  export default {
+    methods: {
+      toggleSideBar() {
+        this.$emit('toggle-nav');
+      }
     }
-  }
-};
+  };
 </script>
 
 <style>
