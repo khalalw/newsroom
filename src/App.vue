b21eea3540e31f9cdfc78355212cb32085c1a7bb
Khalal Walker <khalal@clearsparc.com>
2019-05-22 11:41:43 -0700

    update formatting

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <v-app id="app" dark>
+  <v-app dark id="app">
     <Navbar @toggle-nav="toggleNav"/>
 
     <!-- drawer -->
-    <v-navigation-drawer clipped fixed v-model="drawer" app>
+    <v-navigation-drawer app clipped fixed v-model="drawer">
       <v-list dense>
         <v-list-tile>
           <v-list-tile-action>
@@ -28,7 +28,7 @@
     <v-content>
       <v-container fluid>
 
-          <router-view></router-view>
+        <router-view></router-view>
 
       </v-container>
       <!-- hello -->
@@ -38,17 +38,18 @@
 </template>
 
 <script>
-import Navbar from "./components/Navbar";
-export default {
-  name: "app",
-  components: { Navbar },
-  data: () => ({
-    drawer: null
-  }),
-  methods: {
-    toggleNav() {
-      this.drawer = !this.drawer;
+  import Navbar from './components/Navbar';
+
+  export default {
+    name: 'app',
+    components: { Navbar },
+    data: () => ({
+      drawer: null
+    }),
+    methods: {
+      toggleNav() {
+        this.drawer = !this.drawer;
+      }
     }
-  }
-};
+  };
 </script>
--- src/pages/Top.vue
+++ src/pages/Top.vue
@@ -1,42 +1,42 @@
 <template>
 
-  <v-layout row wrap align-content-space-between justify-space-around>
-    <div class="newscard" v-for="(article, index) in topNews.articles" :key="index">
-        <Newscard
-          :image="article.urlToImage"
-          :newsTitle="article.title"
-          :publishTime="article.publishedAt"
-          :articleURL="article.url"
-          :description="article.description"
-        />
+  <v-layout align-content-space-between justify-space-around row wrap>
+    <div :key="index" class="newscard" v-for="(article, index) in topNews.articles">
+      <Newscard
+        :articleURL="article.url"
+        :description="article.description"
+        :image="article.urlToImage"
+        :newsTitle="article.title"
+        :publishTime="article.publishedAt"
+      />
     </div>
   </v-layout>
 </template>
 
 <script>
-import axios from "axios";
-import Newscard from "../components/Newscard";
+  import axios from 'axios'
+  import Newscard from '../components/Newscard'
 
-export default {
-  name: "Top",
-  components: {
-    Newscard
-  },
-  data() {
-    return {
-      topNews: {}
-    };
-  },
-  created: function() {
-    axios
-      .get(
-        "https://newsapi.org/v2/top-headlines?country=us&apiKey=b7937248fde44f8c83c367b292cc827e"
-      )
-      .then(res => {
-        this.topNews = res.data;
-      });
+  export default {
+    name: 'Top',
+    components: {
+      Newscard
+    },
+    data() {
+      return {
+        topNews: {}
+      }
+    },
+    created: function () {
+      axios
+        .get(
+          'https://newsapi.org/v2/top-headlines?country=us&apiKey=b7937248fde44f8c83c367b292cc827e'
+        )
+        .then(res => {
+          this.topNews = res.data
+        })
+    }
   }
-};
 </script>
 
 <style scoped>
